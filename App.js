ca4a6b396b2afe258162e8b8b44c74be60c44725
Poornimamunuswamy <mspoornima30@gmail.com>
2023-03-30 13:49:30 +0530

    Episode_02

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
 const heading1 = React.createElement("h1", { id: "title1" }, "Hello World from React");
 const heading2 = React.createElement("h2", { id: "title2" }, "Hello World");
 const heading = React.createElement("div",{id:"div_container"},[heading1,heading2]);
