7f722040451159d564459131a2aa52e35c6f04f8
Poornimamunuswamy <mspoornima30@gmail.com>
2023-03-30 11:16:31 +0530

    Episode 1

---
+++ App.js
@@ -0,0 +1,6 @@
+const heading1 = React.createElement("h1", { id: "title1" }, "Hello World from React");
+const heading2 = React.createElement("h2", { id: "title2" }, "Hello World");
+const heading = React.createElement("div",{id:"div_container"},[heading1,heading2]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
